017f3863d36273a041fad9f65bcfc462bc44d679
abrahamm <abrahamoss@Mac-Moss.local>
2025-05-13 13:51:41 -0600

    Se realizo la parte 2 del tutorial 1 con correcciones mostradas al inicio de la parte 3 del tutorial

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@
         $pass = "root";
         $db = "proyectophp";
         $conn = mysqli_connect($host, $user, $pass);
-        mysqli_select_db($db);
+        mysqli_select_db($conn, $db);
         return $conn;
     }
 ?>

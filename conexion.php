001bf7759af16bf4d24ee19e5d56fe1f940db667
abrahamm <abrahamoss@Mac-Moss.local>
2025-05-13 13:07:43 -0600

    Se realizo la parte 1 del tutorial 1

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+    function conectar() {
+        $host = "localhost:8889";
+        $user = "root";
+        $pass = "root";
+        $db = "proyectophp";
+        $conn = mysqli_connect($host, $user, $pass);
+        mysqli_select_db($db);
+        return $conn;
+    }
+?>
